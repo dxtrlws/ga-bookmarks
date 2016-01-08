f0aba415879c25a0813f685039885d0f9bf475c8
Dexter Lewis <blckchns@gmail.com>
2016-01-08 18:49:56 -0500

    Add confirmable action to bookmark app

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :bookmarks
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,  :lockable, :zxcvbnable
+         :recoverable, :rememberable, :trackable, :validatable,  :lockable, :zxcvbnable, :confirmable
 
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later
